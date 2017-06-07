51e2b3b720d38c0db1b31bbee3a461958323ef8c
jorjatis <jorgegargo90@gmail.com>
2017-06-07 21:00:29 +0200

    Entidades preparadas
    Preparación de las entidades para poder trabajar con ellas

--- src/BackendBundle/Controller/DefaultController.php
+++ src/BackendBundle/Controller/DefaultController.php
@@ -8,6 +8,18 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('BackendBundle:Default:index.html.twig');
+      // Carga el entityManager, para acceder a las entidades con Doctrine
+      $em = $this->getDoctrine()->getManager();
+      // Usamos el entityManager para sacar un repositorio, por ejemplo de User, un modelo de consultas, para sacar sus propiedades
+      $user_repo = $em->getRepository("BackendBundle:User");
+      // Vamos a encontrar el usuario 1
+      $user = $user_repo->find(1);
+      // Podemos imprimir varios de sus datos como propiedades, usando los metodos getter
+      echo "Bienvenido ".$user->getEmail();
+
+      var_dump($user);
+      die();
+
+      return $this->render('BackendBundle:Default:index.html.twig');
     }
 }
